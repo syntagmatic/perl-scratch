c4a144d89b35fac853646a7a1235ddb76a470e1c
Kai Chang <kai.s.chang@gmail.com>
2015-10-11 16:08:09 -0700

    category expenses, savings rate, total spend

--- expenses.pl
+++ expenses.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 
-# Parses a CSV of transactions, print out amount and description
+# Parses a CSV of transactions
+# Prints spending by category, total spent, and savings rate
 
 use Parse::CSV;
+use Data::Printer;
 
 $parse = Parse::CSV->new(
     file       => 'secret/expenses.csv',
@@ -11,6 +13,22 @@ $parse = Parse::CSV->new(
     names      => 1,
 );
 
+# group expenses by category
 while ( my $line = $parse->fetch ) {
-    print "\$" .(-$$line{"Amount in USD"}) . "\t" . $$line{"Description"} . "\n";
+    my $category = $$line{"Category"};
+    $expenses{$category} -= $$line{"Amount"};
 }
+
+# sort categories by total expense
+foreach $key (sort { $expenses{$b} <=> $expenses{$a} } keys %expenses) {
+  print $expenses{$key} . "\t$key\n";
+}
+print "\n\n";
+
+# compute total expenses and savings rate
+$total = 0;
+foreach $key (keys %expenses) {
+  $total += $expenses{$key};
+}
+print "Total spent: \$" . $total . "\n";
+print "Savings rate: " . sprintf("%.2f", (100*$expenses{"Investments"} / $total)) . "%\n";
