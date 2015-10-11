70f4fade38fee57a3b57c1293e3fc2d1698f379c
Kai Chang <kai.s.chang@gmail.com>
2015-10-11 16:16:12 -0700

    four space indents

--- expenses.pl
+++ expenses.pl
@@ -21,14 +21,14 @@ while ( my $line = $parse->fetch ) {
 # sort categories by total expense
 print "Top spending categories:\n";
 foreach $key (sort { $expenses{$b} <=> $expenses{$a} } keys %expenses) {
-  print $expenses{$key} . "\t$key\n";
+   print $expenses{$key} . "\t$key\n";
 }
 print "\n\n";
 
 # compute total expenses and savings rate
 $total = 0;
 foreach $key (keys %expenses) {
-  $total += $expenses{$key};
+    $total += $expenses{$key};
 }
 print "Total spent: \$" . $total . "\n";
 print "Savings rate: " . sprintf("%.2f", (100*$expenses{"Investments"} / $total)) . "%\n";
