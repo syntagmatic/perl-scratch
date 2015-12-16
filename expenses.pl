ce7eccee1233fc680a6f8624f4bde7f9dfdfec88
Kai Chang <kai.s.chang@gmail.com>
2015-12-16 01:22:03 -0800

    percentages to categories

--- expenses.pl
+++ expenses.pl
@@ -18,17 +18,19 @@ while ( my $line = $parse->fetch ) {
     $expenses{$category} -= $$line{"Amount"};
 }
 
-# sort categories by total expense
-print "Top spending categories:\n";
-foreach $key (sort { $expenses{$b} <=> $expenses{$a} } keys %expenses) {
-    print $expenses{$key} . "\t$key\n";
-}
-print "\n\n";
-
 # compute total expenses and savings rate
 $total = 0;
 foreach $key (keys %expenses) {
     $total += $expenses{$key};
 }
+
+# sort categories by total expense
+print "\nSpending by category:\n\n";
+foreach $key (sort { $expenses{$b} <=> $expenses{$a} } keys %expenses) {
+    print sprintf("%-14s", $key) . "\t" . sprintf("%5.2f", (100*$expenses{$key} / $total)) . "%\t\t" . sprintf("\$%9.2f", $expenses{$key}) . "\n";
+}
+print "\n\n";
+
+# print total expenses and savings rate
 print "Total spent: \$" . $total . "\n";
 print "Savings rate: " . sprintf("%.2f", (100*$expenses{"Investments"} / $total)) . "%\n";
